91dcb96d3e853c57c85b9d2d416b3bf1d3e4aa4c
Mandeep Singh <mandeep.singh@nagarro.com>
2018-05-15 00:32:02 +0530

    heruko initialized

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 /**
- *  Middleware json and url encoding support
+ *  Middleware for json and url encoding support
  */
 app.use(express.json())
 app.use(express.urlencoded({
